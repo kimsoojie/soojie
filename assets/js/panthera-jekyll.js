0884d75d03383a4ebd0fbad5fd32a52f21393ebb
soojie <soojie@soojieui-MacBook-Neo.local>
2026-09-09 01:06:22 +0900

    init

--- assets/js/panthera-jekyll.js
+++ assets/js/panthera-jekyll.js
@@ -62,7 +62,7 @@ jQuery(document).ready(function () {
       var index = Math.floor(Math.random() * sparkles.length);
       var duration = 4200 + Math.random() * 3600;
       var drift = -40 + Math.random() * 80;
-      var size = 1 + Math.random() * 1.1;
+      var size = 1.3 + Math.random() * 1.4;
 
       activeSparkles += 1;
 
